b8576c40edf7f18398f629bc5f7d33fa050642ce
seouldavid <seouldavid98@gmail.com>
2022-12-15 22:27:08 +0800

    Update hii.java

---
+++ hii.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class hii {
+    public static void main(String[] args) {
+        System.out.print("Hi :");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("*** " + scanner.nextLine() + " ***");
+    }
+}
