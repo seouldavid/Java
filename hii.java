168b571d7b9a4746ecdaecd0df2179cde2c515fe
seouldavid <seouldavid98@gmail.com>
2022-12-16 18:04:47 +0800

    Update hii.java

--- hii.java
+++ hii.java
@@ -1,11 +1,39 @@
 import java.util.Scanner;
 public class hii {
     public static void main(String[] args) {
-        System.out.print("imput number to divide by 2 :");
+        System.out.print("""
+            \\  |  /         ___________
+            ____________  \\ \\_# /         |  ___      |       _________
+           |            |  \\  #/          | |   |     |      | = = = = |
+           | |   |   |  |   \\#           | |`v'|     |      |         |
+           |            |    \\#  //       |  --- ___  |      | |  || | |
+           | |   |   |  |     #_//        |     |   | |      |         |
+           |            |  \\ #_/_______  |     |   | |      | |  || | |
+           | |   |   |  |   \\# /_____/ \\ |      ---  |      |         |
+           |            |    \\# |+ ++|  | |  |^^^^^^| |      | |  || | |
+           |            |    \\# |+ ++|  | |  |^^^^^^| |      | |  || | |
+        ^^^|    (^^^^^) |^^^^^#^| H  |_ |^|  | |||| | |^^^^^^|         |
+           |    ( ||| ) |     # ^^^^^^    |  | |||| | |      | ||||||| |
+           ^^^^^^^^^^^^^________/  /_____ |  | |||| | |      | ||||||| |
+                `v'-                      ^^^^^^^^^^^^^      | ||||||| |
+                 || |`.      (__)    (__)                          ( )
+                             (oo)    (oo)                       /---V
+                      /-------\\/      \\/ --------\\             * |  |
+                     / |     ||        ||_______| \\
+                    *  ||W---||        ||      ||  *
+                       ^^    ^^        ^^      ^^
+                Christmas is coming... what do you see in this ASCII ART? : """);
         Scanner scanner = new Scanner(System.in);
         // System.out.print("*** " + scanner.nextLine() + " ***");
-        int num = scanner.nextInt();
-        System.out.print("Result: " + num/2);
+        // int num = scanner.nextInt();
+        // System.out.print("Result: " + num/2);
+        int inputText = scanner.nextInt();
+        if (inputText == 1) {
+            System.out.println("That's right!");
+        }
+        System.out.println(inputText);
+
+
         scanner.close();
     }
 }
