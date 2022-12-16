fa756c02eadd05db3d1c453d250685d3e1549daa
seouldavid <seouldavid98@gmail.com>
2022-12-16 17:39:41 +0800

    Update hii.java

--- hii.java
+++ hii.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
 public class hii {
     public static void main(String[] args) {
-        System.out.print("Hi :");
+        System.out.print("imput number to divide by 2 :");
         Scanner scanner = new Scanner(System.in);
-        System.out.print("*** " + scanner.nextLine() + " ***");
+        // System.out.print("*** " + scanner.nextLine() + " ***");
+        int num = scanner.nextInt();
+        System.out.print("Result: " + num/2);
         scanner.close();
     }
 }
