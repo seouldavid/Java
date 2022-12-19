f371ad0a96a12679b6eafa815cc5cd0aedeeed2e
seouldavid <seouldavid98@gmail.com>
2022-12-19 23:45:59 +0800

    removed unnecessary comments

--- orginalProjects/guessInPicture.java
+++ orginalProjects/guessInPicture.java
@@ -51,47 +51,6 @@ public class guessInPicture {
         if (!checker) {
             System.out.println("You are incorrect!");
         }
-            // switch (inputText) {
-            //     case "deer":
-            //     System.out.println("You are currect!");
-            //     break;
-            //     case "deers":
-            //     System.out.println("You are currect!");
-            //     break;
-            //     case "male deer":
-            //     System.out.println("You are currect!");
-            //     break;
-            //     case "female deer":
-            //     System.out.println("You are currect!");
-            //     break;
-            //     case "tree":
-            //     System.out.println("You are currect!");
-            //     break;
-            //     case "house":
-            //     System.out.println("You are currect!");
-            //     break;
-            //     case "animals":
-            //     System.out.println("You are currect!");
-            //     break;
-            //     case "animal":
-            //     System.out.println("You are currect!");
-            //     break;
-            //     case "building":
-            //     System.out.println("You are currect!");
-            //     break;
-            //     case "buildings":
-            //     System.out.println("You are currect!");
-            //     break;
-            //     case "exit":
-            //     System.out.println("Off");
-            //     loopOrNot = false;
-            //     break;
-            //     default:
-            //     System.out.println("You are wrong!");
-            //     break;
-    
-            // }
-
         }
 
 
