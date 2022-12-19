edae67d21f0c9454432ac9b3102ac8635184a0b2
seouldavid <seouldavid98@gmail.com>
2022-12-19 23:34:54 +0800

    improved code

--- orginalProjects/guessInPicture.java
+++ orginalProjects/guessInPicture.java
@@ -1,10 +1,7 @@
 package orginalProjects;
 import java.util.Scanner;
 public class guessInPicture {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        boolean loopOrNot = true;
-        String asciiPicture = """
+    static final String asciiPicture = """
             \\  |  /         ___________
             ____________  \\ \\_# /         |  ___      |       _________
            |            |  \\  #/          | |   |     |      | = = = = |
@@ -26,6 +23,9 @@ public class guessInPicture {
                     *  ||W---||        ||      ||  *
                        ^^    ^^        ^^      ^^
                 Christmas is coming... what do you see in this ASCII ART? : """;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean loopOrNot = true;
         while (loopOrNot){
             try {
                 Thread.sleep(2000);
@@ -33,47 +33,61 @@ public class guessInPicture {
                 Thread.currentThread().interrupt();
             }
             System.out.print(asciiPicture);
-        String inputText = scanner.next();
-            switch (inputText) {
-                case "deer":
+        String inputText = scanner.nextLine();
+        String[] figures = new String[] {"deer","deers","male deer","female deer","tree","house","animals","animal","building","buildings","exit"};
+        for (String figure: figures) {
+            if (figure.equals("exit")) {
+                System.out.println("Exit program");
+                break;
+            } else if (figure.equals(inputText)) {
                 System.out.println("You are currect!");
                 break;
-                case "deers":
-                System.out.println("You are currect!");
-                break;
-                case "male deer":
-                System.out.println("You are currect!");
-                break;
-                case "female deer":
-                System.out.println("You are currect!");
-                break;
-                case "tree":
-                System.out.println("You are currect!");
-                break;
-                case "house":
-                System.out.println("You are currect!");
-                break;
-                case "animals":
-                System.out.println("You are currect!");
-                break;
-                case "animal":
-                System.out.println("You are currect!");
-                break;
-                case "building":
-                System.out.println("You are currect!");
-                break;
-                case "buildings":
-                System.out.println("You are currect!");
-                break;
-                case "exit":
-                System.out.println("Off");
-                loopOrNot = false;
-                break;
-                default:
+            } else {
                 System.out.println("You are wrong!");
                 break;
-    
             }
+
+        }
+            // switch (inputText) {
+            //     case "deer":
+            //     System.out.println("You are currect!");
+            //     break;
+            //     case "deers":
+            //     System.out.println("You are currect!");
+            //     break;
+            //     case "male deer":
+            //     System.out.println("You are currect!");
+            //     break;
+            //     case "female deer":
+            //     System.out.println("You are currect!");
+            //     break;
+            //     case "tree":
+            //     System.out.println("You are currect!");
+            //     break;
+            //     case "house":
+            //     System.out.println("You are currect!");
+            //     break;
+            //     case "animals":
+            //     System.out.println("You are currect!");
+            //     break;
+            //     case "animal":
+            //     System.out.println("You are currect!");
+            //     break;
+            //     case "building":
+            //     System.out.println("You are currect!");
+            //     break;
+            //     case "buildings":
+            //     System.out.println("You are currect!");
+            //     break;
+            //     case "exit":
+            //     System.out.println("Off");
+            //     loopOrNot = false;
+            //     break;
+            //     default:
+            //     System.out.println("You are wrong!");
+            //     break;
+    
+            // }
 
         }
 
