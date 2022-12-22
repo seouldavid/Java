b6c7b25630d45d5d11f62a408978ae69b0ce2eed
seouldavid <seouldavid98@gmail.com>
2022-12-22 23:19:56 +0800

    launched new OOP-related projects

---
+++ originalProjects/RPG/characters.java
@@ -0,0 +1,18 @@
+package originalProjects.RPG;
+import java.util.Scanner;
+//practicing Object-Oriented-Programming!
+public class characters {
+    String[] inventory;
+    String name;
+    int level;
+    
+    public void levelUp() {
+        this.level += 1;
+    }
+    public void setUpName() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Whats is your name? : ");
+        this.name = scanner.nextLine();
+        scanner.close();
+    }
+}
