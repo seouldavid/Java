8e1064885d7c8e5169dd9d85730efa5fbb844deb
seouldavid <seouldavid98@gmail.com>
2022-12-25 20:51:19 +0800

    changed class name

--- guidedProjects/test.java
+++
@@ -1,9 +0,0 @@
-package guidedprojects;
-import java.util.Arrays;
-import java.util.Scanner;
-public class test {
-    public static void main(String[] args) {
-        String[] hi = new String[] {"dagger"};
-        System.out.println(Arrays.toString(hi));
-    }
-}
