2101127aee6d111cacf6a93f93ec8a3c7e77d587
seouldavid <seouldavid98@gmail.com>
2022-12-25 15:57:50 +0800

    updated scanner input

--- originalprojects/rpg/game.java
+++ originalprojects/rpg/game.java
@@ -1,5 +1,13 @@
 package originalprojects.rpg;
+import java.util.Scanner;
+import originalprojects.rpg.Characters;
+public class Game {
+    Scanner scanner = new Scanner (System.in);
+    System.out.println("Please choose one of the options : createCharacters/exit");
+    try {
+        String option = scanner.nextLine();
+        errorCapture(option);
+    } catch (Exception e) {
 
-public class game {
-    //to be continued..
+    }
 }
