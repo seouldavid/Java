c32d0ee8d2c763a27b637e98c91b0c4b6ed9f1c8
seouldavid <seouldavid98@gmail.com>
2022-12-25 15:47:14 +0800

    Update test.java

--- guidedProjects/test.java
+++ guidedProjects/test.java
@@ -1,7 +1,16 @@
 package guidedprojects;
+import java.util.Scanner;
 public class test {
     public static void main(String[] args) {
-        String text = "adcd";
-        System.out.print(text.substring(0,3));
+        Scanner scanner = new Scanner(System.in);
+        String temp = scanner.nextLine();
+        for (int i = 0; i < temp.length(); i++) {
+            if (temp.charAt(i) == '0' || temp.charAt(i) == ' ' ) {
+            } else {
+                 System.out.println("Arithmetic");
+                 throw new ArithmeticException();
+            }
+        }
+        scanner.close();
     }
 }
