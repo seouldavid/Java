90aaf22c1e64a02ca6abc1f9d6655b5a5db71c27
seouldavid <seouldavid98@gmail.com>
2022-12-29 18:20:55 +0800

    added file integer analyzer

--- Test.java
+++ Test.java
@@ -1,13 +1,24 @@
-import java.io.*;  
-class M{  
- void method() throws IOException{  
- }  
-}  
-class Testthrows4{  
-   public static void main(String args[])throws IOException{//declare exception  
-     M m=new M();  
-     m.method();  
-  
-    System.out.println("normal flow...");  
-  }  
-}  
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+class Test {
+  public static void main(String[] args) {
+    File file = new File("dataset.txt");
+    int total = 0;
+    try(Scanner scanner = new Scanner(file)) {
+      while(scanner.hasNext()) {
+        int temp = scanner.nextInt();
+        if (temp == 0) {
+          break;
+        }
+        if (temp % 2 == 0) {
+          total += 1;
+        }
+      }
+
+    } catch (FileNotFoundException e) {
+      System.out.println("file not found");
+    }
+    System.out.println(total);
+  }
+}
