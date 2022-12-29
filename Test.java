5ce5016512e3e61a6b55f1e2a758f9852f072531
seouldavid <seouldavid98@gmail.com>
2022-12-29 20:34:17 +0800

    updated data

--- Test.java
+++ Test.java
@@ -3,16 +3,13 @@ import java.io.File;
 import java.io.FileNotFoundException;
 class Test {
   public static void main(String[] args) {
-    File file = new File("dataset.txt");
+    File file = new File("dataset4.txt");
     int total = 0;
     try(Scanner scanner = new Scanner(file)) {
       while(scanner.hasNext()) {
         int temp = scanner.nextInt();
-        if (temp == 0) {
-          break;
-        }
-        if (temp % 2 == 0) {
-          total += 1;
+        if (temp > total) {
+          total = temp; 
         }
       }
 
