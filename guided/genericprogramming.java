d12a95fcad5b00faff166a19c33fdcd5e65c567e
seouldavid <seouldavid98@gmail.com>
2023-01-02 11:37:07 +0800

    Update genericprogramming.java

---
+++ guided/genericprogramming.java
@@ -0,0 +1,19 @@
+package guided;
+
+public class Genericprogramming<T> {
+    private T t;
+
+    public void set(T t) {
+        this.t = t;
+    }
+
+    public T get() {
+        return t;
+    }
+
+}
+class Hi {
+    public static void main(String[] args) {
+        
+    }
+}
