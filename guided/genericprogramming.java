6f968be7a4d3cf571b9cee3e5cb963bb986c1e6d
seouldavid <seouldavid98@gmail.com>
2023-01-02 11:42:35 +0800

    Update genericprogramming.java

--- guided/genericprogramming.java
+++ guided/genericprogramming.java
@@ -1,7 +1,12 @@
 package guided;
 
 public class Genericprogramming<T> {
+
     private T t;
+
+    public Genericprogramming(T t){
+        this.t = t;
+    }
 
     public void set(T t) {
         this.t = t;
@@ -12,8 +17,11 @@ public class Genericprogramming<T> {
     }
 
 }
-class Hi {
+class Sentences {
     public static void main(String[] args) {
-        
+        Genericprogramming<String> sentence = new Genericprogramming<>("Nice to meet you, I am David");
+        System.out.println(sentence.get());
+
+
     }
 }
