15152295ca307fe39752634995ddb685ff40d9c5
seouldavid <seouldavid98@gmail.com>
2023-01-04 19:33:02 +0800

    added comment

--- guided/Implementation.java
+++ guided/Implementation.java
@@ -1,10 +1,12 @@
 package guided;
 
 interface Implementation {
+    //public abstract method
     int count(int[] a);
 }
 
 class Number implements Implementation {
+    //Abstract must be implented
     public int count(int[] a) {
         return a.length;
     }
