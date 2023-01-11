7b0ffa658705d30296bca5299a0ed36afa351c87
seouldavid <seouldavid98@gmail.com>
2023-01-11 20:33:56 +0800

    Update Regex.java

--- lessons/Regex.java
+++ lessons/Regex.java
@@ -6,7 +6,7 @@ public class Main {
         String word = new Scanner(System.in).next();
         int count = 0;
 
-        for (int i = 0; i < word.length() - 2; i++) {
+        for (int i = 0; i < word.length() - 2; i++) {// -2 to the length is to remove outOfBoundaryException it's very clever way!
             if (word.substring(i, i + 3).matches("[aeiouy]{3}") ||
                     word.substring(i, i + 3).matches("[bcdfghjklmnpqrstvxzw]{3}")) {
                 count++;
