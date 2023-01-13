2603aaa24d2bd0767aaafa95dff030437765430e
seouldavid <seouldavid98@gmail.com>
2023-01-13 21:40:52 +0800

    Update Test.java

--- lessons/Test.java
+++ lessons/Test.java
@@ -5,8 +5,9 @@ public class Test {
     public static void main(String[] args) {
         //List<String> word = Arrays.asList(scanner.next().split(""));
         List<String> vowels = Arrays.asList("a","e","i","o","u","y");
-        vowels.add(3,"a");
-        System.out.println(vowels);
+        List<String> model = vowels;
+
+        System.out.println(model);
 
     }
 }
